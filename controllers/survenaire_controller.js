abaf364ab31e1f09e1735cf81d5dfd1d9dbdca91
ttejuosho <theycallmeflowz@yahoo.com>
2019-06-27 06:50:44 -0500

    views loaded

--- controllers/survenaire_controller.js
+++ controllers/survenaire_controller.js
@@ -8,6 +8,15 @@ router.get('/', (req, res) => {
     res.redirect('/index');
 });
 
+router.get('/index', (req,res) => {
+    db.Survey.findAll({})
+    .then( (dbSurvey) => {
+        var hbsObject = {
+            survey: dbSurvey
+        };
+        return res.render("index", hbsObject);
+    });
+});
 
 
 module.exports = router;
---
+++ routes/api-routes.js
@@ -0,0 +1,8 @@
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = (app) => {
+    
+}
