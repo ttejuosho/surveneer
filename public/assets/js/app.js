df3684aad829a586a89551ffba8b74ef94834fb2
Tejuosho <ttejuosho@sentinel.com>
2019-07-01 17:28:54 -0500

    checkbox check

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     console.log("App Loaded");
+    $('input#getId').on('change', () => {
+        if ($(this).is(':checked')) {
+            $('#getId').val(true);
+        } else {
+            $('#getId').val(false);
+        }
+    });
 });
