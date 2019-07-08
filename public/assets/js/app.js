60fe0e172dccebf4d1e1ef2fe7517df2ff0bd781
ttejuosho <theycallmeflowz@yahoo.com>
2019-07-08 01:42:52 -0500

    clean up js file

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
     console.log("App Loaded");
-    $('input#getId').on('change', () => {
-        if ($(this).is(':checked')) {
-            $('#getId').val(true);
-        } else {
-            $('#getId').val(false);
-        }
-    });
+
 });
