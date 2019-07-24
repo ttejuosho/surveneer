b0c164fdc4d5bb6802f5046eecca5de84bfde75a
ttejuosho <theycallmeflowz@yahoo.com>
2019-07-24 07:06:35 -0500

    notification method parameter

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,12 +2,12 @@ $(function() {
     console.log("App Loaded");
     type = ['primary', 'info', 'success', 'warning', 'danger'];
     survenaire = {
-        showNotification: function(from, align) {
+        showNotification: function(from, align, msg) {
             color = Math.floor((Math.random() * 4) + 1);
 
             $.notify({
                 icon: "tim-icons icon-bell-55",
-                message: "Welcome to <b>Black Dashboard</b> - a beautiful freebie for every web developer."
+                message: msg
 
             }, {
                 type: type[color],
