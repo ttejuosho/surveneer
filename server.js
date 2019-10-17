ad5a821b77841dd4b978827e55efc3d52095abb2
Taiwo Tejuosho <ttejuosho@outlook.com>
2019-10-17 18:47:05 -0500

    CORS

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const io = require('socket.io')(http, {
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin,
+            "Access-Control-Allow-Origin": "http://surveneer.herokuapp.com",
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
