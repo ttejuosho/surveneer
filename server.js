9f40341a7cb474d2b3a4e37b84e9d10d7e7ee2ac
Taiwo Tejuosho <ttejuosho@outlook.com>
2019-10-17 19:54:34 -0500

    process port

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const io = require('socket.io')(http, {
 });
 
 require('dotenv').config();
-http.listen(8080, '127.0.0.1');
+http.listen(process.env.PORT, '127.0.0.1');
 
 app.use(cors());
 app.options('*', cors());
