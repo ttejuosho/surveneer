94b57a017d1b0112a8de380d75b306127f79d5f9
Taiwo <ttejuosho@outlook.com>
2020-01-05 23:25:43 -0600

    utils

--- config/utils/utils.js
+++ config/utils/utils.js
@@ -3,36 +3,40 @@ const path = require('path');
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-    destination: './uploads/images/',
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    },
+  destination: 'uploads/images/',
+  filename: function(req, file, cb) {
+    // eslint-disable-next-line max-len
+    cb(
+        null,
+        file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 // Init Upload
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1000000 },
-    fileFilter: function(req, file, cb) {
-        checkFileType(file, cb);
-    },
+  storage: storage,
+  limits: {fileSize: 1000000},
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb);
+  },
 }).single('profileImage');
 
 // Check File Type
 // eslint-disable-next-line require-jsdoc
 function checkFileType(file, cb) {
-    // Allowed ext
-    const filetypes = /jpeg|jpg|png|gif/;
-    // Check ext
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // Check mime
-    const mimetype = filetypes.test(file.mimetype);
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
 
-    if (mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: Images Only!');
-    }
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
 }
 
 module.exports = upload;
